389302ec8978525d55aa0c62820e09ad15922f4f
bobermilk <ppcat39@gmail.com>
2023-02-03 21:11:37 +0800

    typos

--- src/etterna2osu.py
+++ src/etterna2osu.py
@@ -65,7 +65,7 @@ def main():
         os.mkdir(TARGET_DIR)
         
     print(bcolors.HEADER+" "*int((TERMINAL_WIDTH()-27)/2)+f"etterna2osu v{APP_VERSION} by bobermilk"+bcolors.ENDC)
-    print(bcolors.HEADER+" "*int((TERMINAL_WIDTH()-81)/2)+"DM milk#6867 on discord for any queries after reading FAQs at https://milkies.ml/"+bcolors.ENDC)
+    print(bcolors.HEADER+" "*int((TERMINAL_WIDTH()-92)/2)+"DM milk#6867 on discord for any queries after reading FAQs at https://milkies.ml/etterna2osu"+bcolors.ENDC)
     print(bcolors.OKBLUE+" "*int((TERMINAL_WIDTH()-78)/2)+"Thank you demi, guil, marc, chxu, senya, gonx, messica for helping me make this"+bcolors.ENDC)
     print()
     print(bcolors.FAIL+" "*int((TERMINAL_WIDTH()-96)/2)+"bobermilk is not liable for any distribution of the converted packs, only upload your own charts"+bcolors.ENDC)
@@ -135,6 +135,7 @@ def main():
                 offset+=user_offset
             else:
                 print("User offset too large, (max 1000 milliseconds)")
+                user_offset=0
         else:
             user_offset=0
 
