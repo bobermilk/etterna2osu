4a3d84178211bbf2a0d117fe3d20d898f37a0282
bobermilk <ppcat39@gmail.com>
2023-04-15 08:55:20 +0800

    sanitize title helper

--- src/util.py
+++ src/util.py
@@ -3,6 +3,7 @@ import zipfile
 import shutil
 import re
 import subprocess
+import unicodedata
 from PIL import Image
 from math import floor, ceil
 from sys import platform, exit
@@ -57,6 +58,22 @@ def cleanup():
     target_folders = [f for f in os.listdir(".") if not os.path.isfile(f)]
     for folder in target_folders:
         shutil.rmtree(folder)
+
+def slugify(value, allow_unicode=True):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 def main(OD, HP, offset, creator, additional_tags, rates, msd_bounds, remove_ln, diff_name_skillset_msd, keep_pitch):
     # print(OD)
